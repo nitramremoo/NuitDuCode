7e79c4a6b02bfcf693cd4318963dccd20012aea7
ArtkiliouTheDev <100967512+ArtkiliouTheDev@users.noreply.github.com>
2023-05-24 09:09:56 +0200

    Update ndc.py

--- ndc.py
+++ ndc.py
@@ -4,19 +4,22 @@ pyxel.init(128, 128, title="Nuit Du Code", fps=30, quit_key=pyxel.KEY_P)
 
 xPlayer1 = 10
 yPlayer1 = 10
+speed = 1
 
 def update():
     
-    global xPlayer1, yPlayer1
+    global xPlayer1, yPlayer1, speed
     
     if (pyxel.btn(pyxel.KEY_UP)):
-        yPlayer1 -= 1
+        yPlayer1 -= 1 * speed
     if (pyxel.btn(pyxel.KEY_DOWN)):
-        yPlayer1 += 1
+        yPlayer1 += 1 * speed
     if (pyxel.btn(pyxel.KEY_RIGHT)):
-        xPlayer1 += 1
+        xPlayer1 += 1 * speed
     if (pyxel.btn(pyxel.KEY_LEFT)):
-        xPlayer1 -= 1
+        xPlayer1 -= 1 * speed
+    if (pyxel.btn(pyxel.KEY_S)):
+        speed += 1
         
 def draw():
     pyxel.cls(0)
