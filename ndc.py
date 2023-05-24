9952749e4589679665c55ec4012a4b88cb4b0df1
ArtkiliouTheDev <100967512+ArtkiliouTheDev@users.noreply.github.com>
2023-05-24 08:38:44 +0200

    Update ndc.py

--- ndc.py
+++ ndc.py
@@ -1,1 +1,16 @@
-import pyxel, time
+import pyxel
+
+global xPlayer = 0
+global yPlayer = 0
+
+pyxel.init(128, 128, title="Nuit Du Code", fps=30, quit_key="pyxel.KEY_P")
+
+def update():
+    if (pyxel.btn(pyxel.KEY_UP) and y > 0):
+        yPlayer -= 1
+    if (pyxel.btn(pyxel.KEY_DOWN) and y < 129):
+        yPlayer += 1
+    if (pyxel.btn(pyxel.KEY_UP) and x < 129):
+        yPlayer += 1
+    if (pyxel.btn(pyxel.KEY_UP) and x > 0):
+        yPlayer -= 1
